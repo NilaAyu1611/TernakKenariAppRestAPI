19a29e1c998099fa7bc879adab40f560731700e1
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 00:31:51 +0700

    Membuat LoginScreen sebagai stf

---
+++ lib/data/presentation/auth/login_screen.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+
+class _LoginScreenState extends State<LoginScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
