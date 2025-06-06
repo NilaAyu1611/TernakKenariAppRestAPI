1a10c14605d3a5d5aa259ac95a63b51eeee913a9
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 22:32:24 +0700

    membuat class indukrepository

---
+++ lib/data/repository/induk_repository.dart
@@ -0,0 +1,9 @@
+
+import 'package:canarry_app1/services/service_http_client.dart';
+
+class IndukRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  IndukRepository(this._serviceHttpClient);
+
+}
