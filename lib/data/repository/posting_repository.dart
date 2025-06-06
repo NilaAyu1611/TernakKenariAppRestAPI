6f7ae090c7cc603f5a43a351d63451b81af2741c
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:09:19 +0700

    kerangka dasar PostingRepository

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,8 @@
+import 'package:canarry_app1/services/service_http_client.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this._serviceHttpClient);
+
+  
+}
