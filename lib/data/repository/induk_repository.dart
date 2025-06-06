f22ec4aaa9c5e1dfd3c230023ec4286182f85334
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 22:34:35 +0700

    membuat fungsi addinduk

--- lib/data/repository/induk_repository.dart
+++ lib/data/repository/induk_repository.dart
@@ -1,9 +1,36 @@
 
+import 'dart:convert';
+
+import 'package:canarry_app1/data/model/request/admin/induk_request_model.dart';
 import 'package:canarry_app1/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class IndukRepository {
   final ServiceHttpClient _serviceHttpClient;
 
   IndukRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetIndukById>> addInduk(
+    IndukRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "admin/induk",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetIndukById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding induk: $e");
+    }
+  }
+
 
 }
