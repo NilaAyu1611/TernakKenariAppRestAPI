f06b4d9a8d9f30418727a9ea2a5077b3b1722a87
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 22:56:01 +0700

    membuat class anakrepository

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,9 @@
+
+import 'package:canarry_app1/services/service_http_client.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+  
+}
