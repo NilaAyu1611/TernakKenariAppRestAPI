15ee5504c731432dc19ef2f31af762e9ed059ac8
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:18:16 +0700

    Implementasi getAllBurung

--- lib/data/repository/posting_repository.dart
+++ lib/data/repository/posting_repository.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:canarry_app1/data/model/request/admin/posting_jual_request_model.dart';
 import 'package:canarry_app1/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canarry_app1/data/model/response/get_all_burung_response_model.dart';
 import 'package:canarry_app1/services/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 
@@ -32,6 +33,23 @@ class PostingRepository {
     } catch (e) {
       log("Error in Add burung: $e");
       return Left("An error occurred while posting burung: $e");
+    }
+  }
+
+  Future<Either<String, GetAllBurungResponseModel>> getAllBurung() async {
+    try {
+      final response = await _serviceHttpClient.get("admin/burung-semua");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final getAllBurung = GetAllBurungResponseModel.fromMap(jsonResponse);
+        return Right(getAllBurung);
+      } else {
+        final jsonResponse = json.decode(response.body);
+        return Left(jsonResponse['message'] ?? "Get All burung failed");
+      }
+    } catch (e) {
+      return Left("An error occurred while getting all burung: $e");
     }
   }
 
