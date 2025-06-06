418061e0081e26405928f150bc8ee878bbc3a4d9
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:16:15 +0700

    Implementasi addPostBurung

--- lib/data/repository/posting_repository.dart
+++ lib/data/repository/posting_repository.dart
@@ -1,8 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:canarry_app1/data/model/request/admin/posting_jual_request_model.dart';
+import 'package:canarry_app1/data/model/response/burung_semua_tersedia_model.dart';
 import 'package:canarry_app1/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class PostingRepository {
   final ServiceHttpClient _serviceHttpClient;
   PostingRepository(this._serviceHttpClient);
 
-  
+  Future<Either<String, BurungSemuaTersediaModel>> addPostBurung(
+    PostingJualRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/posting-jual',
+        requestModel.toMap(),
+      );
+
+      final jsonResponse = json.decode(response.body);
+
+      if (response.statusCode == 201) {
+        final profileResponse = BurungSemuaTersediaModel.fromJson(jsonResponse);
+        log("Add Burung successfully: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Add burung failed");
+      }
+    } catch (e) {
+      log("Error in Add burung: $e");
+      return Left("An error occurred while posting burung: $e");
+    }
+  }
+
 }
