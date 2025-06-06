d64dadb46bf05987b35c0b689bd3e12eb54654b5
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:56:16 +0700

    membuat login_State

--- lib/data/presentation/auth/bloc/login/login_bloc.dart
+++ lib/data/presentation/auth/bloc/login/login_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:canarry_app1/data/model/request/auth/login_request_mode.dart';
+import 'package:canarry_app1/data/model/response/auth_response_model.dart';
 import 'package:meta/meta.dart';
 
 part 'login_event.dart';
