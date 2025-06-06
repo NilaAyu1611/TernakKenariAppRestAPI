d868c77b404d80b3db50354a9f598cca2748dce7
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:34:56 +0700

    kerangka Dasar profileRepository

---
+++ lib/data/repository/profile_buyer_repository.dart
@@ -0,0 +1,8 @@
+import 'package:canarry_app1/services/service_http_client.dart';
+
+class ProfileBuyerRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  ProfileBuyerRepository(this._serviceHttpClient);
+
+}
