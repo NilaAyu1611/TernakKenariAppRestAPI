f4ae744614b1e21ca1a72a22ec3d102086865da2
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:24:41 +0700

    Inisialisasi Repository dan Dependency

---
+++ lib/data/repository/get_all_burung_tersedia_repository.dart
@@ -0,0 +1,7 @@
+import 'package:canarry_app1/services/service_http_client.dart';
+
+class GetAllBurungTersediaRepository {
+  final ServiceHttpClient httpClient;
+
+  GetAllBurungTersediaRepository(this.httpClient);
+}
