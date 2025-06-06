3f30af24682ffb3de141c1761e0f4b7bd1a70c4f
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-06 23:38:38 +0700

    Method addProfileBuyer

--- lib/data/repository/profile_buyer_repository.dart
+++ lib/data/repository/profile_buyer_repository.dart
@@ -1,8 +1,35 @@
+import 'dart:convert';
+
+import 'package:canarry_app1/data/model/request/buyer/buyer_profile_request_model.dart';
+import 'package:canarry_app1/data/model/response/buyer/buyer_profile_response_model.dart';
 import 'package:canarry_app1/services/service_http_client.dart';
+import 'package:dartz/dartz.dart';
 
 class ProfileBuyerRepository {
   final ServiceHttpClient _serviceHttpClient;
 
   ProfileBuyerRepository(this._serviceHttpClient);
+
+  Future<Either<String, BuyerProfileResponseModel>> addProfileBuyer(
+    BuyerProfileRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "buyer/profile",
+        requestModel.toMap(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = BuyerProfileResponseModel.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  } 
 
 }
