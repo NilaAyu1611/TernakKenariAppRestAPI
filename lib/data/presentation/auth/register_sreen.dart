ea2a91bfdddeb0b2a453bc47aab79198413eae1c
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 07:38:44 +0700

    embuat RegisterScreen sbg StatefulWidget

---
+++ lib/data/presentation/auth/register_sreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class RegisterSreen extends StatefulWidget {
+  const RegisterSreen({super.key});
+
+  @override
+  State<RegisterSreen> createState() => _RegisterSreenState();
+}
+
+class _RegisterSreenState extends State<RegisterSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
