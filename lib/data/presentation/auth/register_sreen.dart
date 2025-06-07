83513a327e628a559045558532ae3dcdde56e5e0
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 07:40:18 +0700

    Inisialisasi Controller dan Form State

--- lib/data/presentation/auth/register_sreen.dart
+++ lib/data/presentation/auth/register_sreen.dart
@@ -8,6 +8,30 @@ class RegisterSreen extends StatefulWidget {
 }
 
 class _RegisterSreenState extends State<RegisterSreen> {
+  late final TextEditingController namaController;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final GlobalKey<FormState> _key;
+  bool isShowPassword = false;
+
+  @override
+  void initState() {
+    namaController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    _key = GlobalKey<FormState>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    namaController.dispose();
+    _key.currentState?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
