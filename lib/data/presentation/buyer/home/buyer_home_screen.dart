a825aca84e90219a18fae90759e846fb49fec3c0
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 14:33:06 +0700

    membuat appbar buyerhomescreen

--- lib/data/presentation/buyer/home/buyer_home_screen.dart
+++ lib/data/presentation/buyer/home/buyer_home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:canarry_app1/data/presentation/auth/login_screen.dart';
 import 'package:canarry_app1/data/presentation/buyer/profile/bloc/profile_buyer_bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BuyerHomeScreen extends StatefulWidget {
@@ -18,6 +20,51 @@ class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(        
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            //pakai loading
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: const Text('Konfirmasi'),
+                    content: const Text('Apakah anda yakin ingin keluar?'),
+                    actions: [
+                      CupertinoDialogAction(                        
+                        child: const Text('Batal'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                       CupertinoDialogAction(                        
+                        child: const Text('Keluar'),
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(builder: (context) => const LoginScreen(),),
+                            (route) => false,
+                          );
+                        },
+                      ),                    
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+
+                
+    );
   }
 }
+            
