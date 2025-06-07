bde724006f6f826a81e7ebb9d68665c2c70a97be
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 08:23:56 +0700

    profile buyer state

--- lib/data/presentation/buyer/profile/bloc/profile_buyer_state.dart
+++ lib/data/presentation/buyer/profile/bloc/profile_buyer_state.dart
@@ -1,6 +1,32 @@
 part of 'profile_buyer_bloc.dart';
 
-@immutable
 sealed class ProfileBuyerState {}
 
 final class ProfileBuyerInitial extends ProfileBuyerState {}
+
+final class ProfileBuyerLoading extends ProfileBuyerState {}
+
+final class ProfileBuyerLoaded extends ProfileBuyerState {
+  final BuyerProfileRequestModel profile;
+
+  ProfileBuyerLoaded({required this.profile});
+}
+
+final class ProfileBuyerError extends ProfileBuyerState {
+  final String message;
+
+  ProfileBuyerError({required this.message});
+}
+
+final class ProfileBuyerAdded extends ProfileBuyerState {
+  final BuyerProfileRequestModel profile;
+
+  ProfileBuyerAdded({required this.profile});
+}
+
+final class ProfileBuyerAddError extends ProfileBuyerState {
+  final String message;
+
+  ProfileBuyerAddError({required this.message});
+
+}
