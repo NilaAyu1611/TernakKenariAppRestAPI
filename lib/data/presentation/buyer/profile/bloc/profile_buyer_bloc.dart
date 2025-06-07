e61188ca2d4cfe554e0aedd32dbd1f3e2d819ccf
NilaAyu1611 <nilaayucw@gmail.com>
2025-06-07 08:19:35 +0700

    profile buyer event (bloc)

---
+++ lib/data/presentation/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:canarry_app1/data/model/request/buyer/buyer_profile_request_model.dart';
+import 'package:meta/meta.dart';
+
+part 'profile_buyer_event.dart';
+part 'profile_buyer_state.dart';
+
+class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
+  ProfileBuyerBloc() : super(ProfileBuyerInitial()) {
+    on<ProfileBuyerEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/data/presentation/buyer/profile/bloc/profile_buyer_event.dart
@@ -0,0 +1,13 @@
+part of 'profile_buyer_bloc.dart';
+
+
+sealed class ProfileBuyerEvent {}
+
+class AddProfileBuyerEvent extends ProfileBuyerEvent {
+  final BuyerProfileRequestModel requestModel;
+
+  AddProfileBuyerEvent({required this. requestModel});
+}
+
+class GetProfileBuyerEvent extends ProfileBuyerEvent {}
+
